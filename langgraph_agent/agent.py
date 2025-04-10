4022bae62f5395cfbe7bae8e7baa7d53fabe8892
Eugene Vuong <vuongeugene@gmail.com>
2025-04-10 16:28:39 -0700

    Implement Template Code

--- langgraph_agent/agent.py
+++ langgraph_agent/agent.py
@@ -12,51 +12,24 @@ load_dotenv()
 llm = ChatGroq(
     model="llama-3.3-70b-versatile", temperature=0
 )
-
 tools = [add_todos, remove_todos]
-
 llm_with_tools = llm.bind_tools(tools)
 
 # STORING MESSAGES
 CONVERSATION = []
 
 # FUNCTION TO CALL CHAT
-def send_message(message):
-    CONVERSATION.append(HumanMessage(message))
-    state = {
-        "messages": CONVERSATION,
-    }
-    new_state = graph.invoke(state)
-    print(TODOS)
-    CONVERSATION.extend(new_state["messages"][len(CONVERSATION):])
+# ... Implement Conversation Function
 
 # Edges
 def should_continue(state: MessagesState):
-    messages = state['messages']
-    last_message = messages[-1]
-    # If the LLM makes a tool call, then we route to the "tools" node
-    if last_message.tool_calls:
-        return "tools"
-    # Otherwise, we stop (reply to the user)
-    return END
+    pass
 
 # Nodes
 def chatbot(state: MessagesState):
-    prompt = SYSTEM_PROMPT + f"\n{json.dumps(TODOS)}"
-
-    return {"messages": [llm_with_tools.invoke([prompt] + state["messages"])]}
+    pass
 
 tools = ToolNode(tools)
 
-# Graph 
-workflow = StateGraph(MessagesState)
-
-# Add Nodes
-workflow.add_node("chatbot", chatbot)
-workflow.add_node("tools", tools)  # Add this line
-
-# Add Edges
-workflow.add_edge(START, "chatbot")
-workflow.add_conditional_edges("chatbot", should_continue)
-workflow.add_edge("tools", "chatbot")
-graph = workflow.compile()
+# Implement Graph Here
+# ...
--- langgraph_agent/tools.py
+++ langgraph_agent/tools.py
@@ -8,19 +8,8 @@ TODOS = []
 
 @tool
 def add_todos(todos: list[str]):
-    """Add new todos to the list"""
-    for todo in todos:
-        TODOS.append({
-            "content": todo,
-            "finished": False,
-            "id": str(uuid.uuid4())
-        })
-    return TODOS
-
+    pass
 
 @tool
 def remove_todos(ids: list[str]):
-    """Remove todos by their IDs"""
-    for todo_id in ids:
-        TODOS[:] = [todo for todo in TODOS if todo["id"] != todo_id]
-    return TODOS
+    pass
